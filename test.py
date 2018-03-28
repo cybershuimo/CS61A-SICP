9e0d45a440874b225f370da86b506f2d0a6501d3
S F <27873085+cybershuimo@users.noreply.github.com>
2018-03-28 16:50:33 +0800

    Hw01 Q4: calling expression will evalute all its operands before evaluating the final result

--- test.py
+++ test.py
@@ -1,1 +1,58 @@
-print("Hello!")
+def pressure(v, t, n):
+    """Compute the pressure in pascals of an ideal gas.
+
+    Applies the ideal gas law: http://en.wikipedia.org/wiki/Ideal_gas_law
+
+    v -- volume of gas, in cubic meters
+    t -- absolute temperature in degrees kelvin
+    n -- particles of gas
+    """
+    k = 1.38e-23  # Boltzmann's constant
+    return n * k * t / v
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    """
+    >>> with_if_statement()
+    1
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    return False
+
+def t():
+    return 0
+
+def f():
+    return 1
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
